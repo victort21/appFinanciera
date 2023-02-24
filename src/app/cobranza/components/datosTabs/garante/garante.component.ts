3ca81acf2f67d64dd510c9a4ce526326bdde928a
= <=>
2023-02-24 12:38:22 -0600

    add: garante tab content

--- src/app/cobranza/components/datosTabs/garante/garante.component.ts
+++ src/app/cobranza/components/datosTabs/garante/garante.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'src/app/clientes/services/cliente.service';
 
 @Component({
   selector: 'app-garante',
   templateUrl: './garante.component.html'
 })
 export class GaranteComponent {
+
+  constructor(public clienteService: ClienteService) {}
 
 }
