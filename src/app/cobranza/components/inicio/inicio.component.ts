8165307719a402fc67d494b2115361a5cb8225d1
= <=>
2023-02-24 08:25:41 -0600

    add: inicio component design

--- src/app/cobranza/components/inicio/inicio.component.ts
+++ src/app/cobranza/components/inicio/inicio.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
-  templateUrl: './inicio.component.html'
+  templateUrl: './inicio.component.html',
+  styles: [`
+    main {
+      min-height: 100vh;
+      max-width: 40%;
+      margin: 0 auto;
+    }
+  `]
 })
 export class InicioComponent {
 
