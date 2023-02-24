bb42c62d7f82e687b305d5adb226dc2edca20e20
= <=>
2023-02-24 12:28:05 -0600

    add: datosCliente tab content

--- src/app/cobranza/components/datosTabs/datos-cliente/datos-cliente.component.ts
+++ src/app/cobranza/components/datosTabs/datos-cliente/datos-cliente.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'src/app/clientes/services/cliente.service';
 
 @Component({
   selector: 'app-datos-cliente',
   templateUrl: './datos-cliente.component.html'
 })
 export class DatosClienteComponent {
+
+  constructor(public clienteService: ClienteService) {}
 
 }
