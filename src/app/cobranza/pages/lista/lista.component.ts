55688568c4a317416b32049c937553291b44d0a0
= <=>
2023-02-24 09:14:40 -0600

    add: lista component design

--- src/app/cobranza/pages/lista/lista.component.ts
+++ src/app/cobranza/pages/lista/lista.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClienteService } from 'src/app/clientes/services/cliente.service';
 
 @Component({
   selector: 'app-lista',
-  templateUrl: './lista.component.html'
+  templateUrl: './lista.component.html',
+  styles: [`
+    @media(min-width: 600px) {
+      section {
+        max-width: 70%;
+      }
+    }
+    @media(min-width: 992px) {
+      section {
+        max-width: 50%;
+      }
+    }
+    .max-w-400 {
+      max-width: 380px;
+    }
+  `]
 })
-export class ListaComponent {
+export class ListaComponent implements OnInit {
+
+  clientes: any;
+
+  constructor(public clienteService: ClienteService) {}
+  ngOnInit(): void {
+    this.clienteService.getClientes().subscribe(cliente => this.clientes = cliente);
+  }
+  
+
 
 }
