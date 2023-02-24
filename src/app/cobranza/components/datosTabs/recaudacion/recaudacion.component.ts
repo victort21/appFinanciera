0bfd1aa16af0b958c7c401972c31484a67a940f1
= <=>
2023-02-24 12:00:01 -0600

    add: recaudacion tab content

--- src/app/cobranza/components/datosTabs/recaudacion/recaudacion.component.ts
+++ src/app/cobranza/components/datosTabs/recaudacion/recaudacion.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ClienteService } from 'src/app/clientes/services/cliente.service';
 
 @Component({
   selector: 'app-recaudacion',
   templateUrl: './recaudacion.component.html'
 })
 export class RecaudacionComponent {
+
+  constructor(public clienteService: ClienteService) {}
 
 }
